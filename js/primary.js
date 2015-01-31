eeaaa8b24c0555a13ece1af4fe67fb661b77b0ce
Johnson Dinh <johnson.dinh@mail.utoronto.ca>
2015-01-31 01:29:05 -0500

    Attempting to parse xml file.

--- js/primary.js
+++ js/primary.js
@@ -1,10 +1,17 @@
 $.ajax({
 	url: 'xml/events.xml',
 	dataType: 'xml',
-	success: function() {
+	success: function(data) {
+		$(data).find('viewentries viewentry').each(function() {
+			var entry = $(this).find('entrydata').attr('EventName').text();
 
+			$('.events ul').append($('<li />', {
+				text:entry
+			}))
+
+		});
 	},
 	error: function() {
-		$('.title').text('Failed to get data!');
+		$('.events').text('Failed to get data!');
 	}
 });
