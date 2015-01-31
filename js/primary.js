2226bb6322fbbe1c3c4cc68b85dc63d83ac76083
Li <lily951159@gmail.com>
2015-01-31 03:56:50 -0500

    tryyy

--- js/primary.js
+++ js/primary.js
@@ -3,16 +3,6 @@ $.ajax({
 	dataType: 'xml',
 	success: function(data) {
 		$(data).find('viewentries viewentry').each(function() {
-			// Empty.
-		});
-	},
-	error: function() {
-		$('.events').text('Failed to get data!');
-	}$.ajax({
-	url: 'xml/events.xml',
-	dataType: 'xml',
-	success: function(data) {
-		$(data).find('viewentries viewentry').each(function() {
 			// Do nothing.
 		}		
 	},
@@ -29,20 +19,3 @@ $('#submit').on('click', function(e) {
 
 	document.getElementById("display").innerHTML = budget + " is your budget.";
 };
-});
-
-$(document.ready(function() {
-	$('#submit').click(function() {
-		alert(1);
-	})
-}))
-
-		/*
-		<script>
-			function nothing() {
-				var budget, date;
-				budget = document.getElementById("form1").elements["budget"].value;
-				date = document.getElementById("form2").elements["date"].value;
-				alert(1);
-			}
-		</script>*/
