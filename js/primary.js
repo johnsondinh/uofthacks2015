3f06892c323a2e2473269ac0e693a9453141daac
Johnson Dinh <johnson.dinh@mail.utoronto.ca>
2015-01-31 09:34:29 -0500

    Say what?

--- js/primary.js
+++ js/primary.js
@@ -6,35 +6,21 @@ $(document).ready(function() {
 		date = x.elements["date"].value;
 		document.getElementById("displayBudget").innerHTML = '$' + budget + '.00';
 		document.getElementById("displayDate").innerHTML = date;
-		//event.preventDefault();
-
-		$.ajax({
-			url: 'xml/events.xml',
-			dataType: 'xml',
-			success: function(data) {
-				$(data).find('viewentries viewentry').each(function() {
-					var entry = $(this).find('entrydata[name = EventName').text();
-
-					$('.events jumbotron ul').append($('<li>', {text: entry}));
-				});
-			},
-			error: function() {
-				$('.events').text('Failed to get data!');
-			}
-		});
-
+		event.preventDefault();
 	});		
 });
 
-/*$.ajax({
+$.ajax({
 	url: 'xml/events.xml',
 	dataType: 'xml',
 	success: function(data) {
 		$(data).find('viewentries viewentry').each(function() {
-			// Empty.
+			var entry = $(this).find('entrydata[name = EventName').text();
+
+			$('.events ul').append($('<li/>', {text: entry}));
 		});
 	},
 	error: function() {
 		$('.events').text('Failed to get data!');
 	}
-});*/
+});
