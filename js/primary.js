c482618422227a159ea6b966c5f46e15a58f1038
Johnson Dinh <johnson.dinh@mail.utoronto.ca>
2015-01-31 01:38:18 -0500

    Testing xml loading again.

--- js/primary.js
+++ js/primary.js
@@ -3,7 +3,7 @@ $.ajax({
 	dataType: 'xml',
 	success: function(data) {
 		$(data).find('viewentries viewentry').each(function() {
-			var entry = $(this).find('viewentry[name = EventName > text').text();
+			var entry = $(this).find('viewentry[name = EventName] > text').text();
 
 			$('.events ul').append($('<li />', {
 				text: entry
