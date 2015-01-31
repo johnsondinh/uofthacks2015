484c17083f832730edcb0207f28e60efcd582d08
Johnson Dinh <johnson.dinh@mail.utoronto.ca>
2015-01-31 04:14:07 -0500

    This has to eventually work.

--- js/primary.js
+++ js/primary.js
@@ -11,9 +11,16 @@ $.ajax({
 	}
 });
 
+$(function() {
+	$(".btn btn-info").click(function() {
+		alert(1);
+	});
+});
+
+/*
 function nothing() {
 	var budget, date;
 	budget = document.getElementById("form1").elements["budget"].value;
 	date = document.getElementById("form2").elements["date"].value;
 	alert(1);
-}
+}*/
