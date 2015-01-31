737e7b622c2a5fb243184515c606ddf2f734f7b2
Johnson Dinh <johnson.dinh@mail.utoronto.ca>
2015-01-31 03:47:04 -0500

    Please?

--- js/primary.js
+++ js/primary.js
@@ -3,17 +3,26 @@ $.ajax({
 	dataType: 'xml',
 	success: function(data) {
 		$(data).find('viewentries viewentry').each(function() {
-
-			var entry = $(this).find('entrydata[name = EventName]').text();
-
-			$('.events ul').append(
-				$('<li />', {
-					text: entry
-				})
-			);
+			// Empty.
 		});
 	},
 	error: function() {
 		$('.events').text('Failed to get data!');
 	}
 });
+
+$(document.ready(function() {
+	$('#submit').click(function() {
+		alert(1);
+	})
+}))
+
+		/*
+		<script>
+			function nothing() {
+				var budget, date;
+				budget = document.getElementById("form1").elements["budget"].value;
+				date = document.getElementById("form2").elements["date"].value;
+				alert(1);
+			}
+		</script>*/
