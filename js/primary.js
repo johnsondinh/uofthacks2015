b3011ac55d04135a521447b07aaf99c8f6f93700
Vibha Kumar <vibhak_95@yahoo.ca>
2015-01-31 12:31:05 -0500

    Trying out the date stuff

--- js/primary.js
+++ js/primary.js
@@ -13,9 +13,15 @@ $(document).ready(function() {
 			dataType: 'xml',
 			success: function(data) {
 				$(data).find('viewentry').each(function() {
-					var name = $(this).find('entrydata[name = EventName]').text();
-					var cost = $(this).find('entrydata[name = Admission]').text();
-
+					//var name = $(this).find('entrydata[name = EventName]').text();
+					//var cost = $(this).find('entrydata[name = Admission]').text();
+					var date_start = $(this).find('entrydata[name = DateBeginShow]');
+					var date_end = $(this).find('entrydata[name = DateEndShow]');
+					var date_start1 = new Date(date_start);
+					var date_start2 = new Date(date_start);
+					if (date >= date_start1 && date <= date_start2){
+						text = $(this).text();
+					}
 					$('.events ul').append($('<li/>', {text: name + cost}));
 				});
 			},
