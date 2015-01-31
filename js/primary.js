9bcf53157d8e0995030c82c26b66d6eebdfb8d16
Johnson Dinh <johnson.dinh@mail.utoronto.ca>
2015-01-31 17:20:39 -0500

    Hello!

--- js/primary.js
+++ js/primary.js
@@ -56,14 +56,9 @@ $(document).ready(function() {
 							cost = "No pricing available.";
 						}
 
-						$('.events ul').append($('<strong/><h3/>')).text(name);
-						//$('.events ul h3').text(name);
-
-						$('.events ul').append($('<h4/>'));
-						$('.events ul h4').text(cost);
-
-						$('.events ul').append($('<p/>')).text(desc);
-						//$('.events ul p').text(desc);
+						$('.events ul').append($('<strong/><h3/>'), {text: name});
+						$('.events ul').append($('<h4/>'), {text: cost});
+						$('.events ul').append($('<p/>'), {text: desc});
 					}
 				});
 			},
