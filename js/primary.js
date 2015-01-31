94860c9713c3099bcca110f0d20caec32921a9f3
Johnson Dinh <johnson.dinh@mail.utoronto.ca>
2015-01-31 08:35:28 -0500

    Tried moving script into a different file.

--- js/primary.js
+++ js/primary.js
@@ -1,3 +1,15 @@
+$(document).ready(function() {
+	$("#submit1").on('click', function(event) {
+		var x, budget, date, y, budget2, month2, date2;
+		x = document.getElementById("form1");
+		budget = x.elements["budget"].value;
+		date = x.elements["date"].value;
+		document.getElementById("displayB").innerHTML = '$' + budget + '.00';
+		document.getElementById("displayD").innerHTML = date;
+		event.preventDefault();
+	});		
+});
+
 $.ajax({
 	url: 'xml/events.xml',
 	dataType: 'xml',
